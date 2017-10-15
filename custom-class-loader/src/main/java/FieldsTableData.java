5a0baaa2815c3c3e115c639ae14714415a9b1882
piotrmajcher94 <piotr.majcher94@gmail.com>
2017-10-15 18:51:37 +0200

    Added gui

---
+++ custom-class-loader/src/main/java/FieldsTableData.java
@@ -0,0 +1,189 @@
+import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+public class FieldsTableData extends AbstractTableModel{
+	
+	private static final String[] COLUMN_NAMES = {"Modifier", "Type", "Name", "Value"};
+	private List<Field> fieldsList;
+	private Class<?> dataClass;
+	private Object relatedClassObject;
+	
+	
+	public Class<?> getDataClass() {
+		return dataClass;
+	}
+	
+	public void createObject() throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException {
+		if (dataClass != null) {
+			Constructor<?> constructor = dataClass.getConstructor();
+			relatedClassObject = dataClass.newInstance();
+			
+			Field[] allClassFields = dataClass.getDeclaredFields();			
+			fieldsList.addAll(Arrays.asList(allClassFields));
+		}
+		fireTableDataChanged();
+	}
+	
+	public void setValue(String fieldName, String fieldValue) throws IllegalArgumentException, IllegalAccessException {
+		if (fieldName == null) {
+			throw new IllegalArgumentException("Argument \"fieldName\" cannot be null!");
+		}
+		
+		boolean fieldSet = false;
+		for (Field field : fieldsList) {
+			if (field.getName().equals(fieldName)) {
+				
+				if (field.getType().equals(int.class)) {
+					try {
+						int value = Integer.parseInt(fieldValue);
+						field.set(relatedClassObject, value);
+						fieldSet = true;
+						break;
+					} catch (NumberFormatException e) {
+						throw new IllegalArgumentException("Wrong field value - should be of type \"int\"!");
+					}
+				}
+				
+				if (field.getType().equals(int[].class)) {
+					try {
+						String[] items = fieldValue.replaceAll("\\[","").replaceAll("\\]", "").replaceAll("\\s", "").split(",");
+						int[] values = new int[items.length];
+						for (int i=0; i<values.length; i++) {
+							values[i] = Integer.parseInt(items[i]);
+						}
+						field.set(relatedClassObject, values);
+						fieldSet=true;
+						break;
+					} catch (NumberFormatException e) {
+						throw new IllegalArgumentException("Wrong field value - should be of type \"int[]\"!");
+					}
+				}
+				
+				if (field.getType().equals(String.class)) {
+					field.set(relatedClassObject, fieldValue);
+					fieldSet = true;
+					break;
+				}
+				
+				if (field.getType().equals(String[].class)) {
+					String[] items = fieldValue.replaceAll("\\[","").replaceAll("\\]", "").replaceAll("\\s", "").split(",");
+					field.set(relatedClassObject, items);
+					fieldSet = true;
+					break;
+				}
+			}
+		}
+		
+		if (!fieldSet) {
+			throw new IllegalArgumentException("Unknown field type! Unable to set the field's value!");
+		}
+		fireTableDataChanged();
+	}
+	
+	public void setDataClass(Class<?> dataClass) {
+		this.dataClass = dataClass;
+	}
+
+	public FieldsTableData() {
+		super();
+		this.fieldsList = new ArrayList<>();
+	}
+	
+	@Override
+	public int getColumnCount() {
+		return COLUMN_NAMES.length;
+	}
+
+	@Override
+	public int getRowCount() {
+		return fieldsList.size();
+	}
+
+	@Override
+	public Object getValueAt(int row, int column) {
+		Object result = null;
+		
+		if (row >= 0 && row < fieldsList.size()) {
+			Field field = fieldsList.get(row);
+			field.setAccessible(true);
+			switch(column) {
+				case 0: {
+					int modifier = field.getModifiers();
+					String modifierString = "";
+					if (Modifier.isPublic(modifier)) {
+						modifierString += "public";
+					} else if (Modifier.isPrivate(modifier)) {
+						modifierString += "private";
+					} else if (Modifier.isProtected(modifier)) {
+						modifierString += "protected";
+					} else {
+						modifierString += "unknown modifier";
+					}
+					result = modifierString;
+					break;
+				}
+				case 1: {
+					result = field.getType().getSimpleName();
+					break;
+				}
+				case 2: {
+					result = field.getName();
+					break;
+				}
+				case 3: {
+					if (relatedClassObject != null) {
+						result = null;
+						try {
+							if (field.getType().isArray()) {
+								Object array = field.get(relatedClassObject);
+								if (array != null) {
+									Object[] values = unpackArray(array);
+									result = "[";
+									for (int i= 0; i<values.length; i++) {
+										result += String.valueOf(values[i]);
+										if (i < values.length - 1) {
+											result += ",";
+										}
+									}
+									result += "]";
+								}
+							} else {
+								result = field.get(relatedClassObject);
+							}
+						} catch (IllegalArgumentException e) {
+							e.printStackTrace();
+						} catch (IllegalAccessException e) {
+							e.printStackTrace();
+						}
+					} 
+					break;
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+	private Object[] unpackArray(Object array) {
+		Object[] resultArray = new Object[Array.getLength(array)];
+	    for(int i=0;i<resultArray.length;i++)
+	        resultArray[i] = Array.get(array, i);
+	    return resultArray;
+	}
+	
+	@Override
+	public String getColumnName(int col) {
+		String result = null;
+		if (col >= 0 && col < COLUMN_NAMES.length) {
+			result = COLUMN_NAMES[col];
+		}
+		return result;
+	}
+}
--- custom-class-loader/src/main/java/Main.java
+++ custom-class-loader/src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
@@ -8,46 +11,132 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
 import customclassloader.CustomClassLoader;
 
 public class Main {
-
-	private static final String dummyClassOneName = "dummyclasses.DummyClassOne";
-	private static final String dummyClassTwoName = "dummyclasses.DummyClassTwo";
-	private static final String dummyClassesRoot = "../dummy-classes/bin/";
+	
+	private static final String APP_NAME = "Custom class loader";
+	private static final String DUMMY_CLASS_ONE_NAME = "dummyclasses.DummyClassOne";
+	private static final String DUMMY_CLASS_TWO_NAME = "dummyclasses.DummyClassTwo";
+	private static final String DUMMY_CLASSES_ROOT = "../dummy-classes/bin/";
 	public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		createAndShowGUI();
+	}
+	
+	private static void createAndShowGUI() {
+		ClassLoader classLoader = new CustomClassLoader(DUMMY_CLASSES_ROOT);
 		
-		ClassLoader classLoader = new CustomClassLoader(dummyClassesRoot);
-		URL mainString = Main.class.getResource("Main.class");
-		System.out.println(mainString);
-		Class<?> dummyClassOne = classLoader.loadClass(dummyClassOneName);
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		JFrame mainFrame = new JFrame(APP_NAME);
 		
-		Constructor<?> dummyConstructor = dummyClassOne.getConstructor();
-		Object o = dummyConstructor.newInstance();
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		Field[] allClassFields = dummyClassOne.getDeclaredFields();
+		// main pane
+		JPanel mainPane = new JPanel();
+		mainPane.setLayout(new BoxLayout(mainPane, BoxLayout.PAGE_AXIS));
 		
-		System.out.println("\n-----------------------------------------"
-				+ "\nClass: "  + dummyClassOne.getName());
-		System.out.println("\nPublic fields:");
+		// top pane for class loading input
+		JPanel loadClassPane = new JPanel();
+		loadClassPane.setLayout(new BoxLayout(loadClassPane, BoxLayout.LINE_AXIS));
 		
+		// fields table
+		FieldsTableData fieldsTableData = new FieldsTableData();
+		JTable fieldsTable = new JTable(fieldsTableData);
+		JScrollPane classFieldsTableScrollPane = new JScrollPane(fieldsTable);
 		
-		System.out.println("\nPrivate fields:");
-		for(Field field : allClassFields) {
-			field.setAccessible(true);
-			int modifier = field.getModifiers();
-			String modifierString = "";
-			if (Modifier.isPublic(modifier)) {
-				modifierString += "public";
-			} else if (Modifier.isPrivate(modifier)) {
-				modifierString += "private";
-			} else if (Modifier.isProtected(modifier)) {
-				modifierString += "protected";
-			} else {
-				modifierString += "unknown modifier";
+		JPanel centerPanel = new JPanel();
+		centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.PAGE_AXIS));
+		JLabel classNameLabel = new JLabel("No class loaded");
+		classNameLabel.setLabelFor(classFieldsTableScrollPane);
+		
+		centerPanel.add(classNameLabel);
+		centerPanel.add(classFieldsTableScrollPane);
+		
+		JLabel classNameInputFieldLabel = new JLabel("Enter full class name: ");
+		JTextField classNameInputField = new JTextField(40);
+		classNameInputFieldLabel.setLabelFor(classNameInputField);
+			
+		JPanel fieldSetterPanel = new JPanel();
+		fieldSetterPanel.setLayout(new BoxLayout(fieldSetterPanel, BoxLayout.LINE_AXIS));
+		
+		JLabel fieldNameInputLabel = new JLabel("Field name: ");
+		JLabel fieldValueInputLabel = new JLabel("Field value: ");
+		
+		JTextField fieldNameInput = new JTextField(20);
+		JTextField fieldValueInput = new JTextField(20);
+		fieldNameInputLabel.setLabelFor(fieldNameInput);
+		fieldValueInputLabel.setLabelFor(fieldValueInput);
+		
+		JTextArea infoArea = new JTextArea(15, 60);
+		infoArea.setText("");
+		infoArea.setEditable(false);
+		JScrollPane textAreaScrollPane = new JScrollPane(infoArea);
+		
+		JButton loadClassButton = new JButton("Load class");
+		loadClassButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String className = classNameInputField.getText();
+				try {
+					fieldsTableData.setDataClass(classLoader.loadClass(className));
+					fieldsTableData.createObject();
+					infoArea.append("Class \"" + className + "\" loaded successfully!");
+				} catch (Exception e1) {
+					infoArea.append("\nCannot load class \"" + className + "\". Exception: " + e1.getMessage());
+				}
+				
 			}
-			System.out.println( "modifier: \"" + modifierString + "\", type: \"" + field.getType().getSimpleName() +"\", name: \"" + field.getName() + "\", value: \""+ field.get(o) + "\"");
-		}
+		});
+		
+		JButton setFieldButton = new JButton("Set field value");
+		setFieldButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String fieldName = fieldNameInput.getText();
+				String fieldValue = fieldValueInput.getText();
+				try {
+					fieldsTableData.setValue(fieldName, fieldValue);
+				} catch(Exception e2) {
+					infoArea.append("\nError: " + e2.getMessage());
+				}
+			}
+		});
+		
+		JPanel bottomPanel = new JPanel();
+		bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.PAGE_AXIS));
+		
+		fieldSetterPanel.add(fieldNameInputLabel);
+		fieldSetterPanel.add(fieldNameInput);
+		fieldSetterPanel.add(fieldValueInputLabel);
+		fieldSetterPanel.add(fieldValueInput);
+		fieldSetterPanel.add(setFieldButton);
+		
+		bottomPanel.add(fieldSetterPanel);
+		bottomPanel.add(textAreaScrollPane);
+		
+		loadClassPane.add(classNameInputFieldLabel);
+		loadClassPane.add(classNameInputField);
+		loadClassPane.add(loadClassButton);
+		
+		mainPane.add(loadClassPane);
+		mainPane.add(centerPanel);
+		mainPane.add(bottomPanel);
+		
+		mainFrame.getContentPane().add(mainPane);
+		mainFrame.pack();
+		mainFrame.setVisible(true);
 	}
 
 }
